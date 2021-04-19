de5e6f963175515a166a1775f23c294f55bc7862
ABproductionIT <79978141+ABproductionIT@users.noreply.github.com>
2021-04-20 03:50:53 +0400

    fix bugs

--- Prime update.py
+++ Prime update.py
@@ -1,5 +1,7 @@
 from numba import njit
 #  coment @njit for dont use GPU
+
+
 @njit
 def fast():
     print("Your number", P, "is`")
@@ -8,10 +10,11 @@ def fast():
         if x == 0:
             print("NOT")
             break
+
+
 P = int(input("Input the number>>"))
 fast()
 print("PRIME")
-
 # 2147483647 prime for test 2 min*
 # using numba>> 2147483647 prime for test 4-10s !!!!!!
 # 2147483648 not prime for test 0000001 ms*
